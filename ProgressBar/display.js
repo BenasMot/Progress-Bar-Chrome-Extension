bd3d337d5ba262529f1ff078197d4ceb3393425f
BenasMot <benasmotiejunas.bm@gmail.com>
2021-02-06 17:43:24 +0200

    Removed extention if looking at a single photo

--- ProgressBar/display.js
+++ ProgressBar/display.js
@@ -1,19 +1,21 @@
-var progressContainer = document.createElement("div");
-progressContainer.id = "extension-progress-container";
-progressContainer.style.width = "100%";
-progressContainer.style.height = "8px";
-progressContainer.style.background = "#ccc";
-progressContainer.style.zIndex = 9999;
-progressContainer.style.position = "fixed";
-progressContainer.style.top = 0;
+var isFoto = document.body.children.length == 1 && document.body.firstChild.nodeName == "IMG";
+if (!isFoto){
+    var progressContainer = document.createElement("div");
+    progressContainer.id = "extension-progress-container";
+    progressContainer.style.width = "100%";
+    progressContainer.style.height = "8px";
+    progressContainer.style.background = "#ccc";
+    progressContainer.style.zIndex = 9999;
+    progressContainer.style.position = "fixed";
+    progressContainer.style.top = 0;
 
-var scrollbar = document.createElement("div");
-scrollbar.id = "extension-scrollbar";
-scrollbar.style.height = "8px";
-scrollbar.style.background = "#4caf50";
-scrollbar.style.width = "0%";
-scrollbar.style.zIndex = 9999;
+    var scrollbar = document.createElement("div");
+    scrollbar.id = "extension-scrollbar";
+    scrollbar.style.height = "8px";
+    scrollbar.style.background = "#4caf50";
+    scrollbar.style.width = "0%";
+    scrollbar.style.zIndex = 9999;
 
-progressContainer.appendChild(scrollbar);
-
-document.getElementsByTagName("body")[0].prepend(progressContainer);
+    progressContainer.appendChild(scrollbar);
+    document.body.prepend(progressContainer);
+}
